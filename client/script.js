f29d4fbd84d078e1d8bd695c8a178a419da5e40c
vorobjewsen30-max <vorobjewsen30@gmail.com>
2026-05-01 14:23:32 +0300

    update API to stasya-store render url

--- client/script.js
+++ client/script.js
@@ -1,4 +1,4 @@
-const API = "http://localhost:3000/api";
+const API = "https://stasya-store.onrender.com/api";
 let cart = [];
 async function loadProducts(){const res=await fetch(`${API}/products`);const products=await res.json();const container=document.getElementById("products");products.forEach(p=>{const div=document.createElement("div");div.className="product";div.innerHTML=`<img src="${p.image}" width="100%"/><h3>${p.name}</h3><p>${p.price} ₽</p><button onclick='addToCart(${JSON.stringify(p)})'>Купить</button>`;container.appendChild(div);});}
 function addToCart(product){cart.push(product);renderCart();}
